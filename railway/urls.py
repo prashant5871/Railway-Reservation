be320a4e7506c23ace823564d2da2b9960c99d69
Prashant Kalsariya <chhabilbhaikalsariya@gmail.com>
2024-03-08 16:23:07 +0530

    login and registration module complete

--- railway/urls.py
+++ railway/urls.py
@@ -2,6 +2,8 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('home',views.home,name="home"),
-    path('register',views.register,name="register"),
     path('login',views.login,name="login"),
+    path('register_user',views.register_user,name="register_user"),
+    path('login_user',views.login_user,name="login_user"),
+    path('user_home',views.user_home,name="user_home"),
 ]
--- railway/views.py
+++ railway/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 def home(request) : 
     return render(request,'index.html')
@@ -6,6 +8,67 @@ def home(request) :
 def login(request) : 
     return render(request,'login-register.html')
 
-def register(request):
-    print("hello")
+def register_user(request):
+    flag = False
+    if request.method == 'POST' :
+        first_name = request.POST['firstName']
+        last_name = request.POST['lastName']
+        user_name = request.POST['userName']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = User.objects.create_user(first_name=first_name,last_name=last_name,username=user_name,password=password,email=email)
+        flag = True
+    
+    data = {'passanger':False,'staff':False,'invalid_user':False,'flag':flag}
+    return render(request,'login-register.html',data)
+
+def login_user(request):
+    invalid_user = False
+    staff = False
+    passanger = False
+    if request.method == 'POST' :
+        uname = request.POST['username']
+        password = request.POST['password']
+        # user = authenticate(username=uname,password=password)
+        # try :
+        #     if user is not None :
+        #         if user.is_staff :
+        #             login(request,user)
+        #             staff = True
+        #         elif user :
+        #             login(request,user)
+        #             passanger = True
+        #     else :
+        #         invalid_user = True
+        # except :
+        #     invalid_user = True
+
+        try:
+            user = authenticate(username=uname,password=password)
+            if user is None : 
+                invalid_user = True
+        except:
+            # print("inside the except")
+            invalid_user = True
+        try:
+            if user.is_staff:
+                login(request)
+                staff = True
+            elif user:
+                login(request)
+                passanger=True
+        except Exception as e:
+            # Log the exception for debugging
+            print(f"An exception occurred during authentication: {e}")
+            invalid_user=True
+    data = {'passanger':passanger,'staff':staff,'invalid_user':invalid_user,'flag':False}
+
+    return render(request,'login-register.html',data)
+
+def user_home(request) :
+    return render(request,'user_home.html')
+
+
+
 
